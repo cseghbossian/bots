de752b8b0c06f71ca6f7488f3484a3d4812dcfc7
Celine Seghbossian <celineblaine@icloud.com>
2021-04-22 17:16:48 -0700

    script switches to jobs tab

--- linkedin.py
+++ linkedin.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Initiate the browser and go to LinkedIn
-browser  = webdriver.Chrome(ChromeDriverManager().install());
+
+options = Options();
+options.add_argument("--incognito");
+
+browser  = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options);
 browser.get('https://www.linkedin.com/');
 
 # Implicitly wait for up to 20 seconds for each element
@@ -27,7 +32,13 @@ browser.find_element_by_name('session_password').send_keys(password);
 # Click Log In
 browser.find_element_by_class_name('sign-in-form__submit-button').click();
 
-# Remember Me? -> Click Not Now
-browser.find_element_by_xpath('//*[@id="remember-me-prompt__form-secondary"]/button').click();
+# # Remember Me? -> Click Not Now
+# rmbr = browser.find_element_by_xpath('//*[@id="remember-me-prompt__form-secondary"]/button');
+# if(rmbr):
+#     rmbr.click();
 
 # Click Jobs
+browser.find_element_by_xpath('//*[@id="primary-navigation"]/ul/li[3]').click();
+
+# Quit
+browser.quit();
